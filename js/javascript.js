54bac382a3ad9adfd999830163bb5bbafb9575f3
Victor Treaba <vtreaba@gmail.com>
2017-09-06 18:36:58 -0400

    js

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+$(function() {
+    console.log( "ready!" );
+    
+});
+
+window.addEventListener('load',
+    function() {
+
+        alert('Hello! This site is currently a WIP');
+
+    }, false);
