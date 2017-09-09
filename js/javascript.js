d1b07f85095b4427caad927985dacb277b1a787a
Victor Treaba <vtreaba@gmail.com>
2017-09-09 19:32:43 -0400

    Test 2

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,3 @@
-sessionStorage.redirect = location.href;
-
-(function(){
-    var redirect = sessionStorage.redirect;
-    delete sessionStorage.redirect;
-    if (redirect && redirect !== location.href) {
-        history.replaceState(null, null, redirect);
-        alert("redirect");
-    }
-    else {
-        alert("index");
-    }
-})();
-
 $(function() {
     console.log( "ready!" );
 
@@ -19,7 +5,5 @@ $(function() {
 
 window.addEventListener('load',
     function() {
-
-    alert("Yo WIP why are you here?");
 
     }, false);
