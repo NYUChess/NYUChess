b7e44febb2e94a8712cfaf929d26ecb120eb8fb4
Victor Treaba <vtreaba@gmail.com>
2017-09-09 19:50:55 -0400

    Random test

--- js/javascript.js
+++ js/javascript.js
@@ -7,5 +7,7 @@ window.addEventListener('load',
     function() {
         document.getElementsByClassName("about")[0].addEventListener('click', function (event) {
             alert(event + " clicked about");
+            alert(location.href);
+            sessionStorage.redirect = location.href;
         });
     }, false);
