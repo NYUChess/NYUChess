6c0f80508e9f6dc9595709c2b97d4e65bbd163be
Victor Treaba <vtreaba@gmail.com>
2017-09-09 19:55:49 -0400

    Test

--- js/javascript.js
+++ js/javascript.js
@@ -6,9 +6,7 @@ $(function() {
 window.addEventListener('load',
     function() {
         document.getElementsByClassName("about")[0].addEventListener('click', function (event) {
-            alert(location.href);
-            sessionStorage.redirect = location.href + "/about";
-            var redirect = sessionStorage.redirect;
-            delete sessionStorage.redirect;
+            document.title = location.href + "/about";
+            window.history.pushState({"html":response.html,"pageTitle":response.pageTitle});
         });
     }, false);
