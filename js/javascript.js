862b6b5e93eb283d5edd6c268dad15e194b02aa6
Victor Treaba <vtreaba@gmail.com>
2017-09-09 19:52:01 -0400

    T

--- js/javascript.js
+++ js/javascript.js
@@ -6,8 +6,9 @@ $(function() {
 window.addEventListener('load',
     function() {
         document.getElementsByClassName("about")[0].addEventListener('click', function (event) {
-            alert(event + " clicked about");
             alert(location.href);
             sessionStorage.redirect = location.href;
+            var redirect = sessionStorage.redirect;
+            delete sessionStorage.redirect;
         });
     }, false);
