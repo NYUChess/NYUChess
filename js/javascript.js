06ff4fe8ef6fe939acd93ec3047c9691cd1e810b
Victor Treaba <vtreaba@gmail.com>
2017-09-09 19:57:54 -0400

    Href Tests

--- js/javascript.js
+++ js/javascript.js
@@ -6,7 +6,7 @@ $(function() {
 window.addEventListener('load',
     function() {
         document.getElementsByClassName("about")[0].addEventListener('click', function (event) {
-            document.title = location.href + "/about";
-            window.history.pushState({"html":response.html,"pageTitle":response.pageTitle});
+            document.title = "NYUChess - About";
+            location.href = location.href + "about";
         });
     }, false);
