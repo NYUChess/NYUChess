f10d72ef8c4980066d589db5c45a6429a3dd8402
Victor Treaba <vtreaba@gmail.com>
2017-09-09 19:52:56 -0400

    Test

--- js/javascript.js
+++ js/javascript.js
@@ -7,7 +7,7 @@ window.addEventListener('load',
     function() {
         document.getElementsByClassName("about")[0].addEventListener('click', function (event) {
             alert(location.href);
-            sessionStorage.redirect = location.href;
+            sessionStorage.redirect = location.href + "/about";
             var redirect = sessionStorage.redirect;
             delete sessionStorage.redirect;
         });
