f3f002041da100c97d43d0ded55f02f07baa4fa2
Victor Treaba <vtreaba@gmail.com>
2017-09-09 19:31:01 -0400

    Redir test

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,17 @@
+sessionStorage.redirect = location.href;
+
+(function(){
+    var redirect = sessionStorage.redirect;
+    delete sessionStorage.redirect;
+    if (redirect && redirect !== location.href) {
+        history.replaceState(null, null, redirect);
+        alert("redirect");
+    }
+    else {
+        alert("index");
+    }
+})();
+
 $(function() {
     console.log( "ready!" );
 
