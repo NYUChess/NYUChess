1170c937cd54ef39c90906b56ce7e4e68831dd50
Victor Treaba <vtreaba@gmail.com>
2017-09-10 13:11:09 -0400

    Forget index

--- js/javascript.js
+++ js/javascript.js
@@ -19,8 +19,8 @@ window.addEventListener('load',
                 alert('This page was redirected by 404.html, from the route: ' + redirect);
                 var check = redirect.replace("http://nyuchess.com/","");
                 if(pages.indexOf(check) > -1) {
-                    document.getElementsByClassName(pages[curPage]).style.display = "none";
-                    document.getElementsByClassName(check).style.display = "block";
+                    document.getElementsByClassName(pages[curPage])[0].style.display = "none";
+                    document.getElementsByClassName(check)[0].style.display = "block";
                     onPage(pages.indexOf(check));
                     curPage = pages.indexOf(check);
                 } else {
