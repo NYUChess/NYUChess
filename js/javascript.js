ad7c10f918bacd2e879f598d48b1f0940b0c2efb
Victor Treaba <vtreaba@gmail.com>
2017-09-10 14:51:08 -0400

    loc

--- js/javascript.js
+++ js/javascript.js
@@ -95,10 +95,7 @@ window.addEventListener('load',
         });
 
         window.onpopstate = function(e){
-            console.log(e);
-            if(e.state){
-                alert(location.href);
-            }
+            alert(location.href);
         };
 
         console.log("Setting up click");
