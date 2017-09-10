8e8e718a0b575c3463cbc6deb86ebbb9675f96c7
Victor Treaba <vtreaba@gmail.com>
2017-09-10 10:00:36 -0400

    Added bodies

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,6 @@
 var pages = [];
 var curPage = 0;
 
-(function(){
-    var redirect = sessionStorage.redirect;
-    delete sessionStorage.redirect;
-    if (redirect && redirect !== location.href) {
-        history.replaceState(null, "", redirect);
-        // REMOVE THIS - just showing the redirect route in the UI
-       alert('This page was redirected by 404.html, from the route: ' + redirect);
-    }
-    else {
-        // REMOVE THIS - just showing the redirect route in the UI
-        alert('This page was loaded directly from the index.html file');
-    }
-})();
-
 $(function() {
     console.log( "ready!" );
 
@@ -22,7 +8,26 @@ $(function() {
 
 window.addEventListener('load',
     function() {
+
+        (function(){
+            var redirect = sessionStorage.redirect;
+            delete sessionStorage.redirect;
+            if (redirect && redirect !== location.href) {
+                history.replaceState(null, "", redirect);
+                // REMOVE THIS - just showing the redirect route in the UI
+                alert('This page was redirected by 404.html, from the route: ' + redirect);
+            }
+            else {
+                // REMOVE THIS - just showing the redirect route in the UI
+                alert('This page was loaded directly from the index.html file');
+            }
+        })();
+
         document.getElementsByClassName("about")[0].addEventListener('click', function (event) {
             history.pushState(null, '', '/about');
         });
     }, false);
+
+function loadPage(x) {
+
+}
