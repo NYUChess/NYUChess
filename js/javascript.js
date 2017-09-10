1364cec415e091efaad4fc85801286ce39bd6542
Victor Treaba <vtreaba@gmail.com>
2017-09-10 15:57:32 -0400

    Fixed warnings

--- js/javascript.js
+++ js/javascript.js
@@ -36,35 +36,35 @@ window.addEventListener('load',
 
         console.log("trying click");
 
-        document.getElementsByClassName("aboutBar")[0].addEventListener('click', function(event) {
+        document.getElementsByClassName("aboutBar")[0].addEventListener('click', function() {
             if(location.href !== "http://nyuchess.com/about") {
                 history.pushState(null, '', '/about');
                 loadPage(1);
             }
         });
 
-        document.getElementsByClassName("calendarBar")[0].addEventListener('click', function(event) {
+        document.getElementsByClassName("calendarBar")[0].addEventListener('click', function() {
             if(location.href !== "http://nyuchess.com/calendar") {
                 history.pushState(null, '', '/calendar');
                 loadPage(2);
             }
         });
 
-        document.getElementsByClassName("contactBar")[0].addEventListener('click', function(event) {
+        document.getElementsByClassName("contactBar")[0].addEventListener('click', function() {
             if(location.href !== "http://nyuchess.com/contact") {
                 history.pushState(null, '', '/contact');
                 loadPage(3);
             }
         });
 
-        document.getElementsByClassName("eventsBar")[0].addEventListener('click', function(event) {
+        document.getElementsByClassName("eventsBar")[0].addEventListener('click', function() {
             if(location.href !== "http://nyuchess.com/events") {
                 history.pushState(null, '', '/events');
                 loadPage(4);
             }
         });
 
-        document.getElementsByClassName("formsBar")[0].addEventListener('click', function(event) {
+        document.getElementsByClassName("formsBar")[0].addEventListener('click', function() {
             if(location.href !== "http://nyuchess.com/forms") {
                 history.pushState(null, '', '/forms');
                 loadPage(5);
@@ -72,7 +72,7 @@ window.addEventListener('load',
         });
 
         for(var i = 0; i < document.getElementsByClassName("mainBar").length; i++) {
-            document.getElementsByClassName("mainBar")[i].addEventListener('click', function (event) {
+            document.getElementsByClassName("mainBar")[i].addEventListener('click', function () {
                 if (location.href !== "http://nyuchess.com/") {
                     history.pushState(null, '', '/');
                     loadPage(0);
@@ -80,21 +80,21 @@ window.addEventListener('load',
             });
         }
 
-        document.getElementsByClassName("newsBar")[0].addEventListener('click', function(event) {
+        document.getElementsByClassName("newsBar")[0].addEventListener('click', function() {
             if(location.href !== "http://nyuchess.com/news") {
                 history.pushState(null, '', '/news');
                 loadPage(6);
             }
         });
 
-        document.getElementsByClassName("picturesBar")[0].addEventListener('click', function(event) {
+        document.getElementsByClassName("picturesBar")[0].addEventListener('click', function() {
             if(location.href !== "http://nyuchess.com/pictures") {
                 history.pushState(null, '', '/pictures');
                 loadPage(7);
             }
         });
 
-        window.onpopstate = function(e){
+        window.onpopstate = function(){
             var toPage = location.href;
             var ext = toPage.replace("http://nyuchess.com/", "");
             if(ext === "") {
