2cdcce6b97b68b455fede878f955d7d529da7b0a
Victor Treaba <vtreaba@gmail.com>
2017-09-10 08:36:24 -0400

    Test

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,20 @@
+var pages = [];
+var curPage = 0;
+
+(function(){
+    var redirect = sessionStorage.redirect;
+    delete sessionStorage.redirect;
+    if (redirect && redirect != location.href) {
+        history.replaceState(null, null, redirect);
+        // REMOVE THIS - just showing the redirect route in the UI
+       alert('This page was redirected by 404.html, from the route: ' + redirect);
+    }
+    else {
+        // REMOVE THIS - just showing the redirect route in the UI
+        alert('This page was loaded directly from the index.html file');
+    }
+})();
+
 $(function() {
     console.log( "ready!" );
 
@@ -6,7 +23,6 @@ $(function() {
 window.addEventListener('load',
     function() {
         document.getElementsByClassName("about")[0].addEventListener('click', function (event) {
-            document.title = "NYUChess - About";
             history.pushState(null, '', '/about');
         });
     }, false);
