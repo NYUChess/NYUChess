8a3d3e5665533f528ad5ea66c38bb8a778b3c0bf
Victor Treaba <vtreaba@gmail.com>
2017-09-21 18:24:41 -0400

    API

--- js/javascript.js
+++ js/javascript.js
@@ -200,6 +200,14 @@ window.addEventListener('load',
             }
         }
 
+        var id = 61738;
+        var key = 'u9G1Rt2PsBxbSDyO8i61w-gPXXaEQetClGfeq7v4mkM';
+
+        $.get( '/api/v2/orgs/' + id + '?key=' + key , function(responseText) {
+            console.log(responseText);
+        });
+
+
     }, false);
 
 function loadPage(x) {
