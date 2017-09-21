a0c589ea45a0f64851b97030e435dcf41a8d6a80
Victor Treaba <vtreaba@gmail.com>
2017-09-21 18:27:52 -0400

    full url

--- js/javascript.js
+++ js/javascript.js
@@ -204,7 +204,7 @@ window.addEventListener('load',
         var key = 'u9G1Rt2PsBxbSDyO8i61w-gPXXaEQetClGfeq7v4mkM';
 
         alert("testing");
-        $.get( '/api/v2/orgs/' + id + '?key=' + key , function(responseText) {
+        $.get( 'https://api.orgsync.com/api/v2/orgs/' + id + '?key=' + key , function(responseText) {
             console.log(responseText);
         });
 
