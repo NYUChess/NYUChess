59928690276260163e4b9ca4969facf7124b6573
Victor Treaba <vtreaba@gmail.com>
2017-09-24 22:15:46 -0400

    main

--- js/javascript.js
+++ js/javascript.js
@@ -207,7 +207,7 @@ window.addEventListener('load',
         $.get( 'https://api.engage.nyu.edu/api/v01/orgs/' + id + '?key=' + key , function(responseText) {
             console.log(responseText);
             //Add mission statement to homepage
-            console.log(document.getElementsByClassName("main")[0].firstChild);
+            console.log(document.getElementsByClassName("main")[0]);
         });
 
 
