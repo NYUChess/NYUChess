6fe3824e8524033fa878b2e716a97b28253cd341
Victor Treaba <vtreaba@gmail.com>
2017-09-28 18:29:16 -0400

    Test

--- js/javascript.js
+++ js/javascript.js
@@ -139,6 +139,7 @@ window.addEventListener('load',
         xhr.open("GET", "https://graph.facebook.com/v2.10/194680683893776/members?access_token=EAACBHKPzZBGMBABJoOcpDl0QnT0LhzGbC6gm6WGZB9n8uGS0aIB1ZAtwGfHuU1b18pzqsg9NLTytgVBS34RHjtyniwAXY6ZAcwi1ZBa5doLyRdX02wG8VeqroNPcPTYm80ZCeDg6Jf1yzHdr8Rw2b6dIfwDMRYhBQZD");
         xhr.send();
 
+        console.log("RESPONSE TEXT");
         console.log(xhr.responseText);
 
         function roll() {
