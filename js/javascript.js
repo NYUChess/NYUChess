31512156382d4fd7297807fb428347ee091c2b5c
Victor Treaba <vtreaba@gmail.com>
2017-09-30 00:21:49 -0400

    Tets

--- js/javascript.js
+++ js/javascript.js
@@ -5,7 +5,7 @@ var id = 61738;
 var key = 'u9G1Rt2PsBxbSDyO8i61w-gPXXaEQetClGfeq7v4mkM';
 var admins = {};
 var adminPics = [];
-$.get( 'https://api.engage.nyu.edu/api/v01/orgs/' + id + '?key=' + key , function(responseText) {
+$.get('https://api.engage.nyu.edu/api/v01/orgs/' + id + '?key=' + key, function (responseText) {
     console.log(responseText);
     //Add mission statement to homepage
     document.getElementsByClassName("mission")[0].innerText = responseText["profile_responses"][0]["data"];
@@ -15,86 +15,124 @@ $.get( 'https://api.engage.nyu.edu/api/v01/orgs/' + id + '?key=' + key , functio
 
     var contacts = [];
 
-    for(var i = 0; i < responseText["profile_responses"].length; i ++) {
-        if(responseText["profile_responses"][i]["element"]["name"].indexOf("Net ID") >= 0
-            || responseText["profile_responses"][i]["element"]["name"].indexOf("netID") >= 0) {
-            contacts.push(responseText["profile_responses"][i]["data"]);
-        }
-    }
+    getData(contacts, "President", responseText);
+
     console.log("CONTACTS");
     console.log(contacts);
 });
 
-$(function() {
-    console.log( "ready!" );
+function getData(arr, position, responseText) {
+    var obj = {}
+
+    var dontAsk = [];
+    dontAsk.push(obj);
+
+    for (var i = 0; i < responseText["profile_responses"].length; i++) {
+        if (responseText["profile_responses"][i]["element"]["name"].equals(position + " First Name") >= 0) {
+            var multiple = responseText["profile_responses"][i]["data"].split(" ");
+            while (multiple.length > dontAsk.length) {
+                dontAsk.push({});
+            }
+            for (var j = 0; j < multiple.length; j++) {
+                dontAsk[j]["FN"] = multiple[j];
+            }
+        }
+        if (responseText["profile_responses"][i]["element"]["name"].equals(position + " Last Name") >= 0) {
+            var multiple = responseText["profile_responses"][i]["data"].split(" ");
+            while (multiple.length > dontAsk.length) {
+                dontAsk.push({});
+            }
+            for (var j = 0; j < multiple.length; j++) {
+                dontAsk[j]["LN"] = multiple[j];
+            }
+        }
+        if (responseText["profile_responses"][i]["element"]["name"].equals(position + " Net ID") >= 0) {
+            var multiple = responseText["profile_responses"][i]["data"].split(" ");
+            while (multiple.length > dontAsk.length) {
+                dontAsk.push({});
+            }
+            for (var j = 0; j < multiple.length; j++) {
+                dontAsk[j]["ID"] = multiple[j];
+            }
+        }
+    }
+
+    for (var i = 0; i < dontAsk.length; i++) {
+        arr.push(dontAsk[i]);
+    }
+
+}
+
+$(function () {
+    console.log("ready!");
 
 });
 
 window.addEventListener('load',
-    function() {
+    function () {
 
-        (function(){
+        (function () {
             var redirect = sessionStorage.redirect;
             delete sessionStorage.redirect;
             if (redirect && redirect !== location.href) {
-                var check = redirect.replace("http://nyuchess.com/","");
-                if(pages.indexOf(check) < 0) {
+                var check = redirect.replace("http://nyuchess.com/", "");
+                if (pages.indexOf(check) < 0) {
                     check = "not";
                     redirect = "404"
                 }
                 history.replaceState(null, "", redirect);
                 document.getElementsByClassName(pages[curPage])[0].style.display = "none";
                 document.getElementsByClassName(check)[0].style.display = "block";
-                if(check !== "not") {
+                if (check !== "not") {
                     onPage(pages.indexOf(check));
                 }
                 curPage = pages.indexOf(check);
             }
             else {
-                $(".main").fadeIn("slow", function() {
-                }) ;
+                $(".main").fadeIn("slow", function () {
+                });
                 onPage(0);
             }
         })();
 
         console.log("trying click");
 
-        document.getElementsByClassName("aboutBar")[0].addEventListener('click', function() {
-            if(location.href !== "http://nyuchess.com/about") {
+        document.getElementsByClassName("aboutBar")[0].addEventListener('click', function () {
+            if (location.href !== "http://nyuchess.com/about") {
                 history.pushState(null, '', '/about');
                 loadPage(1);
             }
         });
 
-        document.getElementsByClassName("calendarBar")[0].addEventListener('click', function() {
-            if(location.href !== "http://nyuchess.com/calendar") {
+        document.getElementsByClassName("calendarBar")[0].addEventListener('click', function () {
+            if (location.href !== "http://nyuchess.com/calendar") {
                 history.pushState(null, '', '/calendar');
                 loadPage(2);
             }
         });
 
-        document.getElementsByClassName("contactBar")[0].addEventListener('click', function() {
-            if(location.href !== "http://nyuchess.com/contact") {
+        document.getElementsByClassName("contactBar")[0].addEventListener('click', function () {
+            if (location.href !== "http://nyuchess.com/contact") {
                 history.pushState(null, '', '/contact');
                 loadPage(3);
             }
         });
 
-        document.getElementsByClassName("eventsBar")[0].addEventListener('click', function() {
-            if(location.href !== "http://nyuchess.com/events") {
+        document.getElementsByClassName("eventsBar")[0].addEventListener('click', function () {
+            if (location.href !== "http://nyuchess.com/events") {
                 history.pushState(null, '', '/events');
                 loadPage(4);
             }
         });
 
-        document.getElementsByClassName("formsBar")[0].addEventListener('click', function() {
-            if(location.href !== "http://nyuchess.com/forms") {
+        document.getElementsByClassName("formsBar")[0].addEventListener('click', function () {
+            if (location.href !== "http://nyuchess.com/forms") {
                 history.pushState(null, '', '/forms');
                 loadPage(5);
             }
         });
 
-        for(var i = 0; i < document.getElementsByClassName("mainBar").length; i++) {
+        for (var i = 0; i < document.getElementsByClassName("mainBar").length; i++) {
             document.getElementsByClassName("mainBar")[i].addEventListener('click', function () {
                 if (location.href !== "http://nyuchess.com/") {
                     history.pushState(null, '', '/');
@@ -103,27 +141,27 @@ window.addEventListener('load',
             });
         }
 
-        document.getElementsByClassName("newsBar")[0].addEventListener('click', function() {
-            if(location.href !== "http://nyuchess.com/news") {
+        document.getElementsByClassName("newsBar")[0].addEventListener('click', function () {
+            if (location.href !== "http://nyuchess.com/news") {
                 history.pushState(null, '', '/news');
                 loadPage(6);
             }
         });
 
-        document.getElementsByClassName("picturesBar")[0].addEventListener('click', function() {
-            if(location.href !== "http://nyuchess.com/pictures") {
+        document.getElementsByClassName("picturesBar")[0].addEventListener('click', function () {
+            if (location.href !== "http://nyuchess.com/pictures") {
                 history.pushState(null, '', '/pictures');
                 loadPage(7);
             }
         });
 
-        window.onpopstate = function(){
+        window.onpopstate = function () {
             var toPage = location.href;
             var ext = toPage.replace("http://nyuchess.com/", "");
-            if(ext === "") {
+            if (ext === "") {
                 ext = "main";
             }
-            if(ext === "404") {
+            if (ext === "404") {
                 ext = "not";
             }
             loadPage(pages.indexOf(ext));
@@ -135,9 +173,9 @@ window.addEventListener('load',
 
         var token = "EAACBHKPzZBGMBABJoOcpDl0QnT0LhzGbC6gm6WGZB9n8uGS0aIB1ZAtwGfHuU1b18pzqsg9NLTytgVBS34RHjtyniwAXY6ZAcwi1ZBa5doLyRdX02wG8VeqroNPcPTYm80ZCeDg6Jf1yzHdr8Rw2b6dIfwDMRYhBQZD";
 
-        $.get('https://graph.facebook.com/v2.10/194680683893776/events?access_token=' + token, function(responseText) {
+        $.get('https://graph.facebook.com/v2.10/194680683893776/events?access_token=' + token, function (responseText) {
             var max = 5;
-            if(responseText["data"].length < 5) {
+            if (responseText["data"].length < 5) {
                 max = responseText["data"].length;
             }
             createEvent(responseText, 0, max);
@@ -156,13 +194,13 @@ window.addEventListener('load',
         }
 
         function getAdmins(url) {
-            $.get(url, function(responseText) {
-                for(var i = 0; i < responseText["data"].length; i++) {
-                    if(responseText["data"][i]["administrator"]) {
-                        admins[responseText["data"][i]["name"]] = responseText["data"][i]["id"] ;
+            $.get(url, function (responseText) {
+                for (var i = 0; i < responseText["data"].length; i++) {
+                    if (responseText["data"][i]["administrator"]) {
+                        admins[responseText["data"][i]["name"]] = responseText["data"][i]["id"];
                     }
                 }
-                if(responseText["paging"]["next"]) {
+                if (responseText["paging"]["next"]) {
                     getAdmins(responseText["paging"]["next"]);
                 }
             });
@@ -218,8 +256,8 @@ window.addEventListener('load',
                 }
                 event.id = "fadeIn";
                 document.getElementsByClassName("events")[0].appendChild(event);
-                $("#fadeIn").fadeIn("fast", function() {
-                }) ;
+                $("#fadeIn").fadeIn("fast", function () {
+                });
                 event.id = "";
             }
 
@@ -231,7 +269,7 @@ window.addEventListener('load',
                 document.getElementsByClassName("events")[0].lastChild.style.marginBottom = "4vw";
                 div.className = "loadMore text-center";
                 div.innerText = "Load Older Posts";
-                div.addEventListener('click', function() {
+                div.addEventListener('click', function () {
                     $.get('https://graph.facebook.com/v2.10/194680683893776/events?access_token=' + token, function (responseText) {
                         var start = document.getElementsByClassName("events")[0].childNodes.length - 1;
                         var max = Math.min(start + 2, responseText["data"].length - 1);
@@ -248,13 +286,13 @@ window.addEventListener('load',
 function loadPage(x) {
     console.log("fading out page");
     disable();
-    $("." + pages[curPage]).fadeOut("slow", function() {
+    $("." + pages[curPage]).fadeOut("slow", function () {
         offPage(curPage);
         onPage(x);
-        $("." + pages[x]).fadeIn("slow", function() {
+        $("." + pages[x]).fadeIn("slow", function () {
             curPage = x;
             enable();
-        }) ;
+        });
     });
 }
 
