0f509e94a6c1aaf5a2b6035a79c8b0e5c2c14c1b
Victor Treaba <vtreaba@gmail.com>
2017-11-22 22:13:56 -0500

    oop

--- js/javascript.js
+++ js/javascript.js
@@ -534,7 +534,6 @@ window.addEventListener('load',
                 });
                 document.getElementsByClassName("pictures")[0].appendChild(div);
                 div.id = "fadeIn";
-                document.getElementsByClassName("events")[0].appendChild(event);
                 $("#fadeIn").fadeIn("fast", function () {
                 });
                 div.id = "";
